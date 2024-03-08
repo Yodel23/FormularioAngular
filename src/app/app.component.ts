dbfbfc041b7daf29c8462e078bbed6a5b853065a
Jose Antonio Muñoz Sarmiento <dawjosele@gmail.com>
2024-03-08 13:50:18 +0100

    Proyecto Angular Formulario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+
+import { ReactiveFormsModule, FormControl, FormGroup } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
-  title = 'FormularioAngular';
+  datos='';
+
+  formularioContacto = new FormGroup({
+    nombre: new FormControl(''),
+    mail: new FormControl(''),
+    mensaje: new FormControl('')
+  });
+
+  submit() {
+    this.datos = `Nombre=${this.formularioContacto.value.nombre}
+                Mail=${this.formularioContacto.value.mail}
+                Mensaje=${this.formularioContacto.value.mensaje}
+                `;
+  }
 }
